2c0b47db6340d375149d9c8a35986400fe39b03f
chiraN-jeevI3013 <87750639+chiraN-jeevI3013@users.noreply.github.com>
2023-03-23 11:09:57 +0530

    Add files via upload

---
+++ vault/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view>
+    <nav>
+      
+    </nav>
+  </router-view>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
